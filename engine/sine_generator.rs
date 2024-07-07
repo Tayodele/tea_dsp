871e2ba272861a5b48c55be1c4d4c234edee52c1
TJ Ayodele <teatimesupreme@gmail.com>
2024-07-07 10:41:32 -0700

    test tweaks

--- engine/sine_generator.rs
+++ engine/sine_generator.rs
@@ -24,19 +24,20 @@ impl Component for SineGeneratorComponent {
         self.freq_hz = control_params.freq_hz();
         self.phase_rad = control_params.phase_rad();
         self.channel_steps.clear();
-        for ch in 0..frame.channels {
+        for _ in 0..frame.channels {
             self.channel_steps.push(0);
         }
         let wave = |step: usize| {
-            let rad = (2.0 * PI) * ((step + 1) as f32) * (self.freq_hz / frame.sample_rate);
+            let rad = (2.0 * PI) * (step as f32) * (self.freq_hz / frame.sample_rate);
+            let rad = rad + self.phase_rad;
             rad.sin() * CEILING
         };
         // All samples start at 0 degrees
         let mut channel_mod = 0;
-        for (idx, sample) in frame.samples.iter_mut().enumerate() {
-            *sample = self.gain_db.attenuate(wave(
-                self.channel_steps[channel_mod] + ((self.phase_rad / 2.0 * PI) as usize),
-            ));
+        for sample in frame.samples.iter_mut() {
+            *sample = self
+                .gain_db
+                .attenuate(wave(self.channel_steps[channel_mod]));
             self.channel_steps[channel_mod] += 1;
             channel_mod = (channel_mod + 1) % (frame.channels as usize);
         }
@@ -53,30 +54,36 @@ mod tests {
     };
     use flatbuffers::FlatBufferBuilder;
 
-    fn rad_to_step(freq: f32, sample_rate: f32, rad: f32) -> usize {
+    fn rad_to_step(freq: f32, sample_rate: f32, rad: f32, channels: usize) -> usize {
         let steps_per_pi = sample_rate / freq;
-        ((rad / (2.0 * PI)) * steps_per_pi) as usize - 1
+        (((rad / (2.0 * PI)) * steps_per_pi) as usize) * channels
     }
 
     #[test]
     fn test_sine_generator_simple() -> anyhow::Result<()> {
+        let channels: u16 = 1;
+        let phase_rad: f32 = PI / 3.0;
+        let gain: f32 = -3.0;
+        let sample_rate = 1200.0;
+        let freq = 100.0;
+
         let mut builder = FlatBufferBuilder::with_capacity(1024);
 
         let control = SineGeneratorControl::create(
             &mut builder,
             &SineGeneratorControlArgs {
-                gain_db: 0.0,
-                freq_hz: 100.0,
-                phase_rad: 0.0,
+                gain_db: gain,
+                freq_hz: freq,
+                phase_rad: phase_rad,
             },
         );
-        let frame_buffer = vec![0.0_f32; 1200];
+        let frame_buffer = vec![0.0_f32; 1024];
         let samples = builder.create_vector(frame_buffer.as_slice());
         let sample_frame = SampleFrame::create(
             &mut builder,
             &SampleFrameArgs {
-                channels: 1,
-                sample_rate: 1200.0,
+                channels: channels as i16,
+                sample_rate: sample_rate,
                 samples: Some(samples),
             },
         );
@@ -92,12 +99,17 @@ mod tests {
 
         let chunk = root_as_chunk(builder.finished_data())?;
         let mut component = SineGeneratorComponent::default();
-        let mut frame = Frame::new(1200.0, 1, 1024);
+        let mut frame = Frame::new(sample_rate, 1, 1024);
         frame.parse_chunk(&chunk)?;
 
         component.process_chunk(&chunk, &mut frame)?;
-        let expected_max_step = rad_to_step(component.freq_hz, frame.sample_rate, PI / 2.0);
-        assert_eq!(frame.samples[expected_max_step], f32::MAX);
+        let expected_max_step = rad_to_step(
+            component.freq_hz,
+            frame.sample_rate,
+            (PI / 2.0) - phase_rad,
+            channels as usize,
+        );
+        assert_eq!(frame.samples[expected_max_step], gain.attenuate(f32::MAX));
 
         Ok(())
     }
